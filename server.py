7f6db45b6e6467e38e9fd6d8127d3cb7eae772bd
yanghunjung1970-alt <yanghunjung1970@gmail.com>
2026-03-15 17:25:35 +0700

    Update server.py
    fix mcp server

--- server.py
+++ server.py
@@ -1,12 +1,10 @@
 from fastmcp import FastMCP
 from datetime import datetime
+
 mcp = FastMCP(name="Datetime-MCP")
 
 @mcp.tool()
 def get_current_datetime() -> str:
-    """현재 날짜와 시간을 반환한다."""
+    """Return current datetime"""
     now = datetime.now()
     return now.strftime("%Y-%m-%d %H:%M:%S")
-
-if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=8000)
